f1f4e4e5f34abd0af63d6cedbe7aafd5eddba579
Tao Guo <tao@temporal.io>
2025-07-20 20:34:47 +1000

    Test custom model provider

--- openai_agents/run_worker.py
+++ openai_agents/run_worker.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 
 import asyncio
+import os
 from datetime import timedelta
+from agents import (
+    Model,
+    ModelProvider,
+    OpenAIChatCompletionsModel,
+    OpenAIResponsesModel,
+    set_tracing_disabled,
+)
 
 from temporalio.client import Client
 from temporalio.contrib.openai_agents import (
@@ -9,6 +17,7 @@ from temporalio.contrib.openai_agents import (
     ModelActivityParameters,
     set_open_ai_agent_temporal_overrides,
 )
+from openai import AsyncOpenAI
 from temporalio.contrib.pydantic import pydantic_data_converter
 from temporalio.worker import Worker
 
@@ -19,6 +28,25 @@ from openai_agents.workflows.hello_world_workflow import HelloWorldAgent
 from openai_agents.workflows.research_bot_workflow import ResearchWorkflow
 from openai_agents.workflows.tools_workflow import ToolsWorkflow
 
+BASE_URL = os.getenv("EXAMPLE_BASE_URL") or ""
+API_KEY = os.getenv("EXAMPLE_API_KEY") or ""
+MODEL_NAME = os.getenv("EXAMPLE_MODEL_NAME") or ""
+
+if not BASE_URL or not API_KEY or not MODEL_NAME:
+    raise ValueError(
+        "Please set EXAMPLE_BASE_URL, EXAMPLE_API_KEY, EXAMPLE_MODEL_NAME via env var or code."
+    )
+
+client = AsyncOpenAI(base_url=BASE_URL, api_key=API_KEY)
+set_tracing_disabled(disabled=True)
+
+
+class CustomModelProvider(ModelProvider):
+    def get_model(self, model_name: str | None) -> Model:
+        return OpenAIChatCompletionsModel(model=model_name or MODEL_NAME, openai_client=client)
+
+
+CUSTOM_MODEL_PROVIDER = CustomModelProvider()
 
 async def main():
     with set_open_ai_agent_temporal_overrides(
@@ -43,7 +71,7 @@ async def main():
                 AgentsAsToolsWorkflow,
             ],
             activities=[
-                ModelActivity().invoke_model_activity,
+                ModelActivity(model_provider=CUSTOM_MODEL_PROVIDER).invoke_model_activity,
                 get_weather,
             ],
         )
